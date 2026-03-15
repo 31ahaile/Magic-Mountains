0a35039722c49d14104c12c85408ea7496bc997b
31ahaile <31ahaile@castilleja.org>
2026-03-15 09:57:10 -0700

    Update Popup.js

--- Popup.js
+++ Popup.js
@@ -1,17 +1,26 @@
-function openPopup(title, image, text) {
+function openPopup(title, image, text, audio) {
+
   document.getElementById("popup-title").innerText = title;
+
   document.getElementById("popup-image").src = image;
+
   document.getElementById("popup-text").innerText = text;
+
+  document.getElementById("popup-audio-source").src = "audio/" + audio;
+
+  document.getElementById("popup-audio").load();
+
   document.getElementById("popup").style.display = "flex";
+
 }
+
 
 function closePopup() {
-  document.getElementById("popup").style.display = "none";
-}
 
-window.addEventListener("click", function(e) {
-  const popup = document.getElementById("popup");
-  if (e.target === popup) {
-    popup.style.display = "none";
-  }
-});
+  document.getElementById("popup").style.display = "none";
+
+  document.getElementById("popup-audio").pause();
+
+  document.getElementById("popup-audio").currentTime = 0;
+
+}
