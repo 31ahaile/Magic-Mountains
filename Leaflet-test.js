161d27dd85cce0a4c08d9915d431f643aba2fa44
31ahaile <31ahaile@castilleja.org>
2026-04-19 17:43:45 -0700

    Add files via upload

---
+++ Leaflet-test.js
@@ -0,0 +1,39 @@
+
+const map = L.map('map').setView([30, -20], 2);
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+}).addTo(map);
+
+
+
+// Jackson Hole
+L.marker([43.7904, -110.6818])
+  .addTo(map)
+  .bindPopup("Grand Teton National Park");
+
+// Tuscany 
+L.marker([43.3407, 11.0439])
+  .addTo(map)
+  .bindPopup("Tuscany");
+
+// Liberia, Costa Rica
+L.marker([10.6346, -85.4407])
+  .addTo(map)
+  .bindPopup("Liberia, Costa Rica");
+
+// Lake Tahoe
+L.marker([39.0968, -120.0324])
+  .addTo(map)
+  .bindPopup("Lake Tahoe");
+
+// Kiawah Island
+L.marker([32.6082, -80.0848])
+  .addTo(map)
+  .bindPopup("Kiawah Island, South Carolina");
+
+// Snoqualmie
+L.marker([47.5287, -121.8250])
+  .addTo(map)
+  .bindPopup("Snoqualmie, Washington");
